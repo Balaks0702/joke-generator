637dbd979c21f2d766aed8be2c23494dc64e5cf1
Balaks0702 <gayathriks0702@gmail.com>
2025-02-08 07:01:41 +0530

    updated styles

--- src/Joke.js
+++ src/Joke.js
@@ -1,19 +1,20 @@
 import React from "react";
 import Button from './Button';
+import './App.css';
 
 const Joke = () => {
     const [Joke, setJoke] = React.useState("");
 
     const renderJoke = () => {
-        fetch("https://sv443.net/jokeapi/v2/joke/Programming?type=single")
+        fetch("https://v2.jokeapi.dev/joke/Programming")
             .then((res) => res.json())
             .then((data) => setJoke(data.joke));
     };
 
     return (
         <div>
-            <Button callApi={renderJoke} />
             <p>{Joke}</p>
+            <Button callApi={renderJoke} />           
         </div>
     );
 }
